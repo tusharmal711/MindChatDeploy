bef4218b6035d9cf7bb7106589ab79ef8ce03a11
tusharmal711 <tusharmal711@gmail.com>
2025-04-02 17:16:04 +0530

    d

--- chat/src/Pages/Chatboard.js
+++ chat/src/Pages/Chatboard.js
@@ -430,7 +430,7 @@ useEffect(() => {
     const newAboutMap={};
     for (const contact of filteredContacts) {
       try {
-        const dpRes = await fetch(`${backendUrl}/api/user?mobile=${contact.mobile}`);
+        const dpRes = await fetch(`${backendUrl}api/fetchDp?mobile=${contact.mobile}`);
         // const dpRes = await fetch(`${backendUrl}api/fetchDp`, {
         //   method: "POST",
         //   headers: { "Content-Type": "application/json" },
