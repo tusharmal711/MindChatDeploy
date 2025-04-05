4f40c471f9115150f092bb6e41d4473da28805e9
tusharmal711 <tusharmal711@gmail.com>
2025-04-05 12:20:37 +0530

    d

--- server/userController.js
+++ server/userController.js
@@ -168,19 +168,19 @@ export const sendLoginOTP = async (req, res) => {
       to: user.email,
       subject: "Mind Chat - OTP Verification",
      
-      html: `<div style="font-family: Arial, sans-serif; line-height: 1.5; max-width: 600px; margin: auto; box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;  overflow: hidden;">
+      html: `<div style="font-family: Arial, sans-serif; line-height: 1.5; max-width: 600px; margin: auto;   overflow: hidden;">
   <div style="padding: 20px; text-align: center;">
-    <img src="cid:unique-content-id" alt="Mind Chat Logo" style="max-width: 150px; height : 100px; width:100px">
+    <img src="cid:unique-content-id" alt="Mind Chat Logo" style="max-width: 150px; height : 13d0px; width:130px">
   </div>
-  <div style="padding: 20px;">
+  <div style="padding: 20px;box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;">
     <h2 style="color: #333;  font-family: sans-serif;">Your OTP Code</h2>
-    <p style="font-family: sans-serif; font-size : 16px">Please use the following OTP code to complete your verification:</p>
+    <p style="font-family: sans-serif; font-size : 15px;  color:rgb(77, 77, 77);">Please use the following OTP code to complete your verification:</p>
     <div style="font-size: 24px; font-weight: bold; margin: 20px 0; color: #007BFF;">
       ${otp}
     </div>
-    <p style="font-family: sans-serif; font-size : 16px">This code is valid for the next 5 minutes.</p>
-    <p style="font-family: sans-serif; font-size : 16px">If you did not request this code, please ignore this email.</p>
-    <p style="font-family: sans-serif; font-size : 16px">Thank you</p>
+    <p style="font-family: sans-serif; font-size : 15px;  color:rgb(77, 77, 77); ">This code is valid for the next 5 minutes.</p>
+    <p style="font-family: sans-serif; font-size : 15px;  color:rgb(77, 77, 77);">If you did not request this code, please ignore this email.</p>
+    <p style="font-family: sans-serif; font-size : 15px;  color:rgb(77, 77, 77);">Thank you</p>
     </div>
 </div>`,
 
