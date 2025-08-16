429acaafcc4e429f707b49244af822370cbcbc2c
tusharmal711 <tusharmal711@gmail.com>
2025-08-16 22:19:52 +0530

    d

--- chat/src/Pages/CallPage.js
+++ chat/src/Pages/CallPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import io from "socket.io-client";
 import { GoDotFill } from "react-icons/go";
@@ -173,7 +173,7 @@ useEffect(() => {
 
   // If the caller hears that someone joined, they start the call
   socket.on("user-joined", () => {
-  startCall();
+
   setStatus("Connected");
   setIsConnected(true);
   });
