092021a4f4586c97d54c09de50d7708def2db159
tusharmal711 <tusharmal711@gmail.com>
2025-08-18 14:05:41 +0530

    d

--- chat/src/Pages/Socket.js
+++ chat/src/Pages/Socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001/", {
+export const socket = io("https://mindchatdeploy-2.onrender.com/", {
   transports: ["websocket"], // Forces WebSocket connection
   withCredentials: true, // Allows cross-origin credentials
   reconnection: true,
